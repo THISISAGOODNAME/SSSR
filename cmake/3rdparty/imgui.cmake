fd8fb02ec11d75f071545ea429fa83238d0fb8dc
THISISAGOODNAME <yangyj19931231@gmail.com>
2021-09-25 19:31:10 +0800

    SSSR first commit

---
+++ cmake/3rdparty/imgui.cmake
@@ -0,0 +1,14 @@
+add_library(imgui STATIC
+    ${project_root}/3rdparty/imgui/imgui.cpp
+    ${project_root}/3rdparty/imgui/imgui_demo.cpp
+    ${project_root}/3rdparty/imgui/imgui_draw.cpp
+    ${project_root}/3rdparty/imgui/imgui_tables.cpp
+    ${project_root}/3rdparty/imgui/imgui_widgets.cpp
+    )
+
+target_include_directories(imgui
+    PUBLIC
+    ${project_root}/3rdparty/imgui
+    )
+
+set_target_properties(imgui PROPERTIES FOLDER "3rdparty/imgui")
